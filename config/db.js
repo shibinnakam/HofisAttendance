bc87133bcc7dd50764cbab12050ec55103978c91
shibinnakam <shibinsajinakathunkal@gmail.com>
2026-09-14 20:55:35 +0530

    Add reliable DNS resolver to db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Configure reliable DNS servers to prevent querySrv ECONNREFUSED on local ISPs
+try {
+  dns.setServers(['8.8.8.8', '1.1.1.1', '8.8.4.4']);
+} catch (e) {
+  // Ignore if in environments where setServers is restricted
+}
 
 const connectDB = async () => {
   const uri = process.env.MONGODB_URI;
